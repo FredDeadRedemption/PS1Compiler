3ce4cb9e9faf141656f341d17968550da7fde5c5
Fred_Dead_Redemption <frederikdau@gmail.com>
2024-02-27 14:51:08 +0100

    puha

--- scripts/run_speedtest.bash
+++ scripts/run_speedtest.bash
@@ -1,6 +1,3 @@
 export PATH="$(pwd)/.venv/bin:$PATH"
 
-echo "arg1= "$1
-echo "arg2= "$2
-
-python tests/speedtest.py "$1" "$2" # $1 = sys arg 1
+python tests/speedtest.py "$1" "$2" 
--- tests/speedtest.py
+++ tests/speedtest.py
@@ -2,10 +2,7 @@ import subprocess
 import time
 import sys
 
-print("py ran")
-
 def measure(path_to_exe, sys_arg):
-  print("py " + path_to_exe + sys_arg)
   start = time.time()
   subprocess.run([path_to_exe, sys_arg])  # Run the executable
   stop = time.time()
