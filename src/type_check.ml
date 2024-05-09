f33f24004131f72c5bca792179a534d666cd7453
Emilsp3 <115782475+Emilsp3@users.noreply.github.com>
2024-05-09 22:12:57 +0200

    method og formal typecheck!!!

--- src/type_check.ml
+++ src/type_check.ml
@@ -16,6 +16,12 @@ let check_bool x =
   | Some _ -> failwith("This is not a bool\n")
   | None -> Printf.printf "Not initialized \n"
 
+let check_void x =
+  match x with
+    | Some(Void) -> Printf.printf "This is a void\n"
+    | Some _ -> failwith("This is not a void\n")
+    | None -> Printf.printf("Not initialized \n")
+
 let check_fields fields =
   List.iter (fun field ->
     match field with
@@ -28,18 +34,34 @@ let check_fields fields =
         | _ -> Printf.printf "fail \n"
   ) fields
 
-(*let check_methods methods =
-  List.iter (fun meth ->
-    match meth with
-    | MethodDef(typespec, _, formals, _) ->
-      match typespec with
-        | Int -> check_int (formals)
-        | Float -> check_float (formals)
-        (*| Bool -> check_bool (Some(expr))*)
-        | Generic ""-> Printf.printf "Generic \n"
-        | _ -> Printf.printf "fail \n"
-  ) methods*)
+let check_formal_int typespec =
+  match typespec with
+  | Int -> Printf.printf "This is a formal int\n"
+  | _ -> failwith "this is not an int"
 
+let check_method_int typespec =
+  match typespec with
+  | Int -> Printf.printf "This is a method int\n"
+  | _ -> failwith "this is not an int"
+
+  
+
+let check_methods methods =
+  List.iter (fun meth ->
+      match meth with
+      | MethodDef(typespec, _, formal, _) ->
+        match typespec with 
+        | Int -> check_method_int typespec 
+        | _ -> ();
+        List.iter (fun (formal_typespec, _) ->
+          match formal_typespec with
+          | Int -> check_formal_int formal_typespec
+          | _ -> ()
+      ) formal;
+  ) methods
+
+  
+  
 let check_type (game_class, classes) = 
   match game_class with
   | ClassStmt (_, (fl,_,_,_))-> check_fields fl; 
@@ -47,7 +69,11 @@ let check_type (game_class, classes) =
   List.iter (fun clas ->
     match clas with 
     | ClassStmt (_, (fl, _,_,_)) -> check_fields fl
-    (*| ClassStmt (_, (_,_,_,ml)) -> check_methods ml*)
+  ) classes;
+
+  List.iter (fun clas ->
+    match clas with 
+    | ClassStmt (_, (_, _,_,ml)) -> check_methods ml
   ) classes
   
   
