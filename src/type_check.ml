c0273dbd1407da6accd1788327530c60377d6314
Emilsp3 <115782475+Emilsp3@users.noreply.github.com>
2024-05-16 14:00:39 +0200

    type_check methods done and Class init case handled

--- src/type_check.ml
+++ src/type_check.ml
@@ -3,15 +3,17 @@ let check_int x =
   match x with
   | ConstInt _ -> Printf.printf "This is an int\n"
   | _ -> failwith("Expected int in expression\n")
+
 let check_float x =
   match x with
   | ConstFloat _ -> Printf.printf "This is a float\n"
   | _ -> failwith("Expected float in expression\n")
+
 (*let check_bool x =
   match x with
-  | Bool -> Printf.printf "This is a bool\n"
-  | _ -> failwith("This is not a bool\n")
-*)
+  | Bool _ -> Printf.printf "This is a bool\n"
+  | _ -> failwith("Expected bool in expression\n")*)
+
 let check_fields fields =
   List.iter (fun field ->
     match field with
@@ -20,26 +22,73 @@ let check_fields fields =
         | Int -> check_int expr
         | Float -> check_float expr
         (*| Bool -> check_bool expr*)
-        | Generic ""-> Printf.printf "Generic\n"
-        | _ -> failwith "Wrong field type \n")
+        | Generic _ -> Printf.printf "Generic\n"
+        | _ -> failwith "Wrong field type \n");
+
     | FieldDefU (typ, _) -> 
       (match typ with
-        | Int -> Printf.printf"Uninitialized int\n"
-        | Float -> Printf.printf"Uninitialized float\n"
-        | Bool -> Printf.printf"Uninitialized bool\n"
-        | Generic "" -> Printf.printf"Uninitialized Generic\n"
+        | Int -> Printf.printf "Uninitialized int\n"
+        | Float -> Printf.printf "Uninitialized float\n"
+        | Bool -> Printf.printf "Uninitialized bool\n"
+        | Generic _ -> Printf.printf "Uninitialized Generic\n"
         | _ -> failwith "Wrong field type\n"
-  )) fields
+      );
+    | FieldClsInit (_,_) -> Printf.printf "New class initialised\n"
+  ) fields
+
+let check_method_int list =
+  List.iter (fun stmt ->
+    match stmt with 
+    | ReturnStmt expr ->
+      (match expr with 
+      | ConstInt _ -> Printf.printf "Method int return\n"
+      | _ -> failwith "Return type doesn't match method typespec \n")
+    | _ -> ()
+  ) list
+
+let check_method_float list =
+  List.iter (fun stmt ->
+    match stmt with 
+    | ReturnStmt expr ->
+      (match expr with 
+      | ConstFloat _ -> Printf.printf "Method float return\n"
+      | _ -> failwith "Return type doesn't match method typespec \n")
+    | _ -> ()
+  ) list
+
+let check_method_bool list =
+  List.iter (fun stmt ->
+    match stmt with 
+    | ReturnStmt expr ->
+      (match expr with 
+      | Bool _ -> Printf.printf "Method bool return\n"
+      | _ -> failwith "Return type doesn't match method typespec \n")
+    | _ -> ()
+  ) list
+
+let check_method_void block =
+  List.iter (fun stmt ->
+    match stmt with 
+    | ReturnStmt expr ->
+      (match expr with 
+      | ConstInt _ -> failwith "Expected no return value for void method, but got int\n"
+      | ConstFloat _ -> failwith "Expected no return value for void method, but got float\n"
+      | Bool _ -> failwith "Expected no return value for void method, but got bool\n"
+      | Var _ -> failwith "Expected no return value for void method, but got variable\n"
+      | FuncCall _ -> failwith "Expected no return value for void method, but got function call\n"
+      | _ -> Printf.printf "Expected no return value\n")
+    | _ -> ()
+  ) block
 
 let check_methods methods =
   List.iter (fun metho ->
       match metho with
-      | MethodDef(typespec, _, formal, _) ->
+      | MethodDef(typespec, _, formal, block) ->
         (match typespec with 
-        | Int -> Printf.printf("Int\n")
-        | Float -> Printf.printf("float\n")
-        | Bool -> Printf.printf("Bool\n")
-        | Void -> Printf.printf("Void\n")
+        | Int -> check_method_int block
+        | Float -> check_method_float block
+        | Bool -> check_method_bool block
+        | Void -> check_method_void block
         | _ -> failwith "method typespec error\n" );
         
         List.iter (fun (formal_typespec, _) ->
